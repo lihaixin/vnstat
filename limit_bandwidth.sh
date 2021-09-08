3b7bf2507bd383e0cab7009a074ada972fbb08f6
lihaixin <lihaixin@15099.net>
2021-09-08 22:55:29 +0800

    Update limit_bandwidth.sh

--- limit_bandwidth.sh
+++ limit_bandwidth.sh
@@ -18,19 +18,19 @@ if [ ! -f /tmp/limit ]; then
    if [ "$DATATXTYPE" = "$MAXLIMTYPE" ]; then
         if [ $(bc <<< "$DATATX >= $MAXTX") -eq 1 ]; then
                 echo "WARNING TX bytes bandwidth limit hit!"
-                tc qdisc add dev $INTERFACE root tbf rate 1mbit burst 1kb latency 60ms
+                tc qdisc add dev $INTERFACE root tbf rate 1mbit burst 2kb latency 50ms
                 touch /tmp/limit
         fi
         
         if [ $(bc <<< "$DATAALL >= $MAXALL") -eq 1 ]; then
                 echo "WARNING TX and RX bytes bandwidth limit hit!"
-                tc qdisc add dev $INTERFACE root tbf rate 1mbit burst 100kb latency 60ms
+                tc qdisc add dev $INTERFACE root tbf rate 1mbit burst 2kb latency 50ms
                 touch /tmp/limit
         fi
    fi
 fi
 
 if [ `date +%d` = 01 ]; then
-        tc qdisc del dev $INTERFACE root tbf rate 1mbit burst 100kb latency 60ms
+        tc qdisc del dev $INTERFACE root tbf rate 1mbit burst 2kb latency 50ms
         rm -rf /tmp/limit
 fi
